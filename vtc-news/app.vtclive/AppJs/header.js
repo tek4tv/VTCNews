910950881f4cc523e3fc335f47f0bde8cad3f058
MaiToan71 <maivantoan339@gmail.com>
2020-10-19 17:14:21 +0700

    video player

---
+++ vtc-news/app.vtclive/AppJs/header.js
@@ -0,0 +1,10 @@
+﻿var div = document.getElementsByClassName('backtoHome')[0];
+div.addEventListener('click', function (event) {
+    var android = location.href.match(/#android$/) || navigator.userAgent.match(/Android/i) != null;
+    if (android) {
+        DetailActivity.onBackPress();
+    }
+    else {
+        webkit.messageHandlers.onBackPress.postMessage('');
+    }
+});
--- vtc-news/app.vtclive/AppJs/video.js
+++ vtc-news/app.vtclive/AppJs/video.js
@@ -12,6 +12,10 @@
             return JSON.parse(item);
         }
     };
+    self.scrollToTop = function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
     self.loadData = function () {
         //self.loadGetVideoHomes();
         self.sideBar();
@@ -62,12 +66,83 @@
            
         });
     }
-    
-    self.selectedVideo = ko.observable();   
+
     self.loadSelectedVideo = function (item) {
-        self.mode('video')       
-        self.selectedVideo(item)
-    }  
+        alert(item.Path())
+    }
+   
+    self.getVideoByID = ko.observable();
+    self.loadByVideoByID = function (item) {       
+        var url = window.location.origin + "/VideoDetail/Index/" + item.PrivateID();     
+        if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)) {
+            var android = location.href.match(/#android$/) || navigator.userAgent.match(/Android/i) != null;
+            if (android) {
+                MainActivity.goToDetail(item.Path(), url, 'https://vtcnews.tek4tv.vn/Header/Index', 50, false);
+            } else {
+                webkit.messageHandlers.goToDetail.postMessage(
+                    {
+                        url: item.Path(),
+                        urldetail: url,
+                        urlWvHeader: 'https://vtcnews.tek4tv.vn/Header/Index',
+                        height: 50,
+                        isLive: false,
+                        type: 0,
+                        titleCast: item.Name(),
+                        descriptionCast: item.Description(),
+                        urlImage: self.getImage(item, 'Thumbnail')
+                    });
+            }
+        };
+    }
+    
+    /*
+    self.slugifyLink = function (str) {
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+
+        // remove accents, swap ñ for n, etc
+        var from = "áàảãạăẵẳằắặâẫẩầấậđéẽèẻèêễếềểệíìỉĩịóòõỏọôỗồổốộơỡởờớợúũùủụưữửừứựýỹỷỳỵ·/_,:;";
+        var to = "aaaaaaaaaaaaaaaaadeeeeeeeeeeeiiiiiooooooooooooooooouuuuuuuuuuuyyyyy------";
+        for (var i = 0, l = from.length; i < l; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+
+        return str;
+    }
+    self.gotoItem = function (item) {
+        var url = window.location.origin + '/view/' + self.slugifyLink(item.Name()) + '/' + item.PrivateID() + '.html';
+        if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)) {
+            var android = location.href.match(/#android$/) || navigator.userAgent.match(/Android/i) != null;
+            if (android) {
+                MainActivity.goToDetail(item.Path(), url, 'https://appnow.tek4tv.vn/back.html', 50, false);
+            } else {
+                webkit.messageHandlers.goToDetail.postMessage(
+                    {
+                        url: item.Path(),
+                        urldetail: url,
+                        urlWvHeader: 'https://appnow.tek4tv.vn/back.html',
+                        height: 30,
+                        isLive: false,
+                        type: 0,
+                        titleCast: item.Name(),
+                        descriptionCast: item.Description(),
+                        urlImage: self.getImage(item, 'Thumbnail')
+                    });
+            }
+        };
+    }
+   
+    */
 }
 $(function () {
     ko.cleanNode(document.getElementById("body-content"));
